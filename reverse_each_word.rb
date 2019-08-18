7d388a6507ec41359950e1bbc4286d20f8434c53
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-18 23:48:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
   array = sentence1.split(" ") 
-  array = array.each {|array| array.reverse}
-  array
+  array2 = array.each {|array| array.reverse}
+  array2
 end 
