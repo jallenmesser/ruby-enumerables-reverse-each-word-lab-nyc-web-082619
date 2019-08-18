89fde0f50de890b37667c3da7af9a0458651695e
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-18 23:57:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   array = sentence1.split(" ") 
   array2 = []
-  array.each {|array| array2.push(array.reverse)}
+  array.collect {|array| array2.push(array.reverse)}
   result = array2.join(" ")
 end 
